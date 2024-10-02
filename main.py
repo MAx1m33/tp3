a1a38900cea33e0f6f581b4fc00934623ac911d2
Maxime Le Grand <l19010002@V-PP-47-015.salsa.univ-amu.fr>
2024-10-02 09:28:30 +0200

    jeu fonctionnel

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import random as rd
 
 
+
 class jeu:
 
     def __init__(self, m, n):
         self.k = rd.randint(0, m)
+        print(self.k)
         self.n = n
-        print(k)
+        print(self.k)
         if __name__ == "__main__":
             import doctest
             doctest.testmod()
@@ -21,31 +23,32 @@ class jeu:
     #         import doctest
     #         doctest.testmod()
 
-    def test(self, i, k):
+    def test(self, i):
         self.n = self.n - 1
-        if i == k:
+        if i == self.k:
             print("Bravo, tu as gagné!")
             return True
-        elif i > k:
+        elif i > self.k:
             print("Trop grand!")
             return False
         else:
             print("Trop petit!")
             return False
 
-    def jouer(self):
+    def jouer(self,):
         while self.n > 0:
             try:
                 print("attention, il vous reste ", self.n, " essais")
                 partie = int(input("Entrez un nombre entier : "))
                 if self.test(partie):
                     break
+                if self.n == 0:
+                    print("Tu as perdu!")
             except ValueError:
                 print("Ceci n'est pas un entier, entrez un nouveau nombre")
-        if self.n == 0:
-            print("Tu as perdu!")
         return
 
 
 j = jeu(100, 6)
-k = jeu.jouer()
+k = jeu.jouer(j)
+
